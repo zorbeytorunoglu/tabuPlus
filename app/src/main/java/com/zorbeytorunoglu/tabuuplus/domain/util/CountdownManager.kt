d8c17b7f0c870c829abb199830a7dc9019d7a600
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-10-30 01:29:26 +0300

    Created a CountdownManager

---
+++ app/src/main/java/com/zorbeytorunoglu/tabuuplus/domain/util/CountdownManager.kt
@@ -0,0 +1,59 @@
+package com.zorbeytorunoglu.tabuuplus.domain.util
+
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+
+class CountdownManager(
+    initialTime: Int,
+    private val scope: CoroutineScope
+) {
+
+    private val _remainingTimeFlow = MutableStateFlow(initialTime)
+    val remainingTimeFlow: StateFlow<Int> = _remainingTimeFlow
+
+    private val isPaused = AtomicBoolean(false)
+
+    private var countdownJob: Job? = null
+
+    private var onFinish: (() -> Unit)? = null
+
+    fun start() {
+        countdownJob = scope.launch {
+            while (_remainingTimeFlow.value >= 0) {
+                if (!isPaused.get()) {
+                    if (_remainingTimeFlow.value == 0) {
+                        stop()
+                        onFinish?.invoke()
+                        return@launch
+                    }
+                    _remainingTimeFlow.value = _remainingTimeFlow.value - 1
+                }
+                delay(1000)
+            }
+        }
+    }
+
+    fun isPaused(): Boolean = isPaused.get()
+
+    fun pause() {
+        isPaused.set(true)
+    }
+
+    fun resume() {
+        isPaused.set(false)
+        Log.e("Tabu", "Baslattim")
+    }
+
+    fun stop() = countdownJob?.cancel()
+
+    fun setOnFinish(unit: () -> Unit) {
+        this.onFinish = unit
+    }
+
+}
