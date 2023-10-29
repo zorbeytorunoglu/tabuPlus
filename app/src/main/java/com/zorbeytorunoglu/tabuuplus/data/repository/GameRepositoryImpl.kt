7736d561e19127d52c83ec29de172874b6a07aa4
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-10-30 01:30:03 +0300

    Implemented gameData

--- app/src/main/java/com/zorbeytorunoglu/tabuuplus/data/repository/GameRepositoryImpl.kt
+++ app/src/main/java/com/zorbeytorunoglu/tabuuplus/data/repository/GameRepositoryImpl.kt
@@ -1,11 +1,14 @@
 package com.zorbeytorunoglu.tabuuplus.data.repository
 
 import com.zorbeytorunoglu.tabuuplus.common.Holder
+import com.zorbeytorunoglu.tabuuplus.domain.model.GameData
 import com.zorbeytorunoglu.tabuuplus.domain.model.GameSettings
 import com.zorbeytorunoglu.tabuuplus.domain.repository.GameRepository
 
 class GameRepositoryImpl: GameRepository {
 
     override val gameSettings: Holder<GameSettings> = Holder()
+
+    override val gameData: Holder<GameData> = Holder()
 
 }
--- app/src/main/java/com/zorbeytorunoglu/tabuuplus/domain/repository/GameRepository.kt
+++ app/src/main/java/com/zorbeytorunoglu/tabuuplus/domain/repository/GameRepository.kt
@@ -1,10 +1,13 @@
 package com.zorbeytorunoglu.tabuuplus.domain.repository
 
 import com.zorbeytorunoglu.tabuuplus.common.Holder
+import com.zorbeytorunoglu.tabuuplus.domain.model.GameData
 import com.zorbeytorunoglu.tabuuplus.domain.model.GameSettings
 
 interface GameRepository {
 
     val gameSettings: Holder<GameSettings>
+
+    val gameData: Holder<GameData>
 
 }
