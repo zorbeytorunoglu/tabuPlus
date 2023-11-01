baafce6d292297fdb85cc3313185eaa8ff9c527f
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-11-01 20:19:21 +0300

    Made TeamData serializable

--- app/src/main/java/com/zorbeytorunoglu/tabuuplus/domain/model/TeamData.kt
+++ app/src/main/java/com/zorbeytorunoglu/tabuuplus/domain/model/TeamData.kt
@@ -1,7 +1,9 @@
 package com.zorbeytorunoglu.tabuuplus.domain.model
 
+import java.io.Serializable
+
 data class TeamData(
     val name: String,
     var correctScore: Int = 0,
     var falseScore: Int = 0
-)
+): Serializable
