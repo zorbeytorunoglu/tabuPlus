0b2f9b2c4521f4e81dcfa2d56d8c516b5dd6e906
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-11-07 20:09:05 +0300

    Now the game fragment supports more densities.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.zorbeytorunoglu.tabuuplus"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
